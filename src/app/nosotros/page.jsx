e7401fa5d8f02b86b50f9df85728cfa939db37e1
damian sola <damiansola99@gmail.com>
2025-03-18 13:35:15 -0300

    otra imagen

--- src/app/nosotros/page.jsx
+++ src/app/nosotros/page.jsx
@@ -1,4 +1,6 @@
 import Services from "@/components/services";
+import Image from "next/image";
+import fotoNos from "../../images/IMG_SNS7.HEIC.jpg"
 
 const { default: Footer } = require("@/components/footer")
 const { default: Navbar } = require("@/components/NavBar")
@@ -16,7 +18,8 @@ const AboutUs = () => {
                 </div>
                 <div className="px-6 md:px-20 text-left pb-10" >
                     <h1 className="text-3xl tracking-widest py-6 font-bold">Nosotros</h1>
-                    <p className="md:tracking-wide text-justify md:leading-loose ">
+                    <div className="md:flex items-start">
+                    <p className="md:tracking-wide text-justify md:leading-loose pr-10">
                     SNS Abogados es una organización constituida por profesionales expertos en las distintas áreas del derecho con una misma visión y ética del ejercicio profesional, poniendo el foco en el asesoramiento jurídico personalizado y aplicado mediante un profundo conocimiento del derecho y de los intereses de sus clientes. 
                     SNS Abogados se encuentra compuesto por profesionales del derecho especializados en todas las áreas del Asesoramiento Jurídico, con especial dedicación en las prácticas vinculadas a los litigios judiciales y extrajudiciales; arbitrajes y mediaciones; y el Asesoramiento Jurídico Preventivo e Integral de todas las áreas del Derecho.
                     En el Derecho Laboral, todo lo relacionado al derecho individual y colectivo del trabajo, del empleado y del empleador. Actuaciones ante los distintos órganos Administrativos (Secretaría de Trabajo, Superintendencia de Riesgo de Trabajo, Distintas ART, etc.). Comunicaciones judiciales y extrajudiciales, cómo la correcta representación en la Sede Judicial de los conflictos.
@@ -24,8 +27,9 @@ const AboutUs = () => {
                     En el Derecho Civil, todo los relacionado al Derecho de Familia (Divorcio; Alimentos -Aumento, Disminución y Cese-; División de Bienes; Régimen de Comunicación; Cuidado Personal; Régimen de Visitas; Régimen de Coparentalidad; Régimen del Matrimonio, Uniones Civiles y Concubinato; etc.), Contratos Civiles, Capacidad de las personas (Restricciones), representaciones de sus derechos en Sede Judicial y Extra Judicial, Mediaciones -Privadas y Judiciales- y Arbitrajes.
                     En el Área de Violencia Familiar, en todo lo relativo a su representación en  Sede Judicial, Policial  y Privada.
                     En el Derecho Penal, en todo lo relativo a la defensa de los derechos del clientes, cómo víctima, querellante o imputado, en las actuaciones policiales, de fiscalía, en los distintos Institutos Penitenciarios, etc.
-
                     </p>
+                    <Image src={fotoNos} className='' width={400} height={100} alt='foto-nos'/>
+                    </div>
                 </div>
                 <div className="px-6 md:px-20 text-left pb-10" >
                     <h1 className="text-3xl tracking-widest py-6 font-bold">Nuestra Propuesta</h1>
@@ -35,6 +39,7 @@ const AboutUs = () => {
                 </div> 
                 <div className="px-6 md:px-20 text-left pb-10" >
                     <h1 className="text-3xl tracking-widest py-6 font-bold">El ESTUDIO SNS ABOGADOS -ESTUDIO JURÌDICO INTEGRAL</h1>
+            
                     <p className="md:tracking-wide text-justify md:leading-loose">
                         En febrero del año 2020, los Dres. Jorge Pablo Suárez Nelson y Agustín Federico Simó se asocian y fundan el Estudio Jurídico Integral SNS Abogados. En Octubre del mismo año, se agrega el Ab. Leonardo Martín González López.                      
                         Hoy el Estudio tiene más de cuatro años de trabajo conjunto.
@@ -43,8 +48,8 @@ const AboutUs = () => {
                         Especial consideración merecen por parte de sus integrantes, los aspectos relacionados a la   actividad preventiva, advirtiendo mediante asesoramiento temporáneo de las situaciones conflictivas que pudieren ocurrir, aportando en cada caso las adecuadas soluciones.
                         El Estudio cuenta con una extensa red de colaboradores, en las distintas profesiones, como así también de corresponsales en el  Interior de la Provincia de Salta y el País, a fin de permitir un correcto tratamiento de situaciones que exceden el ámbito territorial de la Ciudad de Salta. 
                         Los invitamos a contactarse, y formular las consultas necesarias para poder ayudarlo en la defensa de sus intereses.
-
                     </p>
+                    
                 </div>
                 <Services/>
             </main>
